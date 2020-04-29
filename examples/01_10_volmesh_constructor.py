e1ed5a27fcd066b79baaaae54c3e10c5f32b9553
duchaoyu <43376817+duchaoyu@users.noreply.github.com>
2020-04-29 23:04:41 +0200

    - update ForceVolMesh from guids (maybe move it somewhere...? / cannot find a better way... ) - update forceartist

--- examples/01_10_volmesh_constructor.py
+++ examples/01_10_volmesh_constructor.py
@@ -3,11 +3,15 @@ from __future__ import print_function
 from __future__ import division
 
 import compas
+from compas.utilities import geometric_key
+from compas_rhino import unload_modules
+unload_modules('compas')
 
-from compas_rhino.geometry._constructors import volmesh_from_polysurfaces
+from compas_rhino.geometry import RhinoSurface
 
 from compas_3gs.diagrams import ForceVolMesh
 from compas_3gs.rhino import draw_cell_labels
+from compas_3gs.datastructures import VolMesh3gsArtist
 
 try:
     import rhinoscriptsyntax as rs
@@ -24,24 +28,42 @@ __email__      = 'juney.lee@arch.ethz.ch'
 # ------------------------------------------------------------------------------
 # 1. make volmesh from rhino polysurfaces
 # ------------------------------------------------------------------------------
-
 # select Rhino polysurfaces
 guids = rs.GetObjects("select polysurfaces", filter=rs.filter.polysurface)
-rs.HideObjects(guids)
 
-# the layer in which the component should be drawn
-layer = 'force_volmesh'
-# construct the volmesh object from Rhino polysurfaces
-forcediagram       = ForceVolMesh()
-forcediagram       = volmesh_from_polysurfaces(forcediagram, guids)
-forcediagram.layer = layer
-forcediagram.attributes['name'] = layer
+# make a volmesh from polysurface
+vertices = []
+cells = []
+for guid in guids:
+    vertices_dict = {}
+    polysurface= RhinoSurface.from_guid(guid) # this function doesn't work for extrusion objects
+    mesh = polysurface.brep_to_compas()
+    for key in mesh.vertices():
+        if mesh.vertex_coordinates(key) not in vertices:
+            vertices_dict[key] = len(vertices)
+            vertices.append(mesh.vertex_coordinates(key))
+        else:
+            vertices_dict[key] = vertices.index(mesh.vertex_coordinates(key))
+    cell = []
+    for fkey in mesh.faces():
+        face = [vertices_dict[vkey] for vkey in mesh.face_vertices(fkey)]
+        cell.append(face)
+    cells.append(cell)
+    
+forcediagram = ForceVolMesh.from_vertices_and_cells(vertices, cells)
+
+rs.HideObjects(guids)
 
 
 # ------------------------------------------------------------------------------
 # 2. visualise volmesh (force diagram)
 # ------------------------------------------------------------------------------
+# draw cells
+layer = 'force_volmesh'
+forceartist = VolMesh3gsArtist(forcediagram, layer=layer)
+forceartist.draw()
+forceartist.draw_vertexlabels()
 
-forcediagram.draw(layer=layer)
-forcediagram.draw_vertex_labels(layer=layer)
+# draw labels in the center of each cell
+forcediagram.layer = layer
 draw_cell_labels(forcediagram)
