bdd410f5b609c0da77038bca3042d59c782d056d
duchaoyu <43376817+duchaoyu@users.noreply.github.com>
2020-05-03 21:42:58 +0200

    - update 01_61

--- examples/01_60_volmesh_modification.py
+++ examples/01_60_volmesh_modification.py
@@ -4,11 +4,15 @@ from __future__ import division
 
 import compas
 
-from compas_rhino.geometry._constructors import volmesh_from_polysurfaces
+from compas_rhino import unload_modules
+unload_modules("compas")
+
+from compas_rhino.geometry import RhinoSurface
 
 from compas_3gs.diagrams import ForceVolMesh
 from compas_3gs.rhino import rhino_vertex_move, rhino_vertex_align, rhino_vertex_modify_fixity
 from compas_3gs.rhino import rhino_volmesh_vertex_lift, draw_vertex_fixities
+from compas_3gs.datastructures import VolMesh3gsArtist
 
 try:
     import rhinoscriptsyntax as rs
@@ -25,19 +29,39 @@ __email__      = 'juney.lee@arch.ethz.ch'
 # ------------------------------------------------------------------------------
 # 1. make vomesh from rhino polysurfaces
 # ------------------------------------------------------------------------------
-
 # select Rhino polysurfaces
 guids = rs.GetObjects("select polysurfaces", filter=rs.filter.polysurface)
-rs.HideObjects(guids)
 
-# construct volmesh (force diagram) from Rhino polysurfaces
-layer = 'force_volmesh'
-forcediagram       = ForceVolMesh()
-forcediagram       = volmesh_from_polysurfaces(forcediagram, guids)
-forcediagram.layer = layer
-forcediagram.attributes['name'] = layer
-# visualise force_volmesh
-forcediagram.draw(layer=layer)
+# make a volmesh from polysurface
+vertices = []
+cells = []
+for guid in guids:
+    vertices_dict = {}
+    polysurface= RhinoSurface.from_guid(guid) # this function doesn't work for extrusion objects
+    mesh = polysurface.brep_to_compas()
+    for key in mesh.vertices():
+        if mesh.vertex_coordinates(key) not in vertices:
+            vertices_dict[key] = len(vertices)
+            vertices.append(mesh.vertex_coordinates(key))
+        else:
+            vertices_dict[key] = vertices.index(mesh.vertex_coordinates(key))
+    cell = []
+    for fkey in mesh.faces():
+        face = [vertices_dict[vkey] for vkey in mesh.face_vertices(fkey)]
+        cell.append(face)
+    cells.append(cell)
+    
+forcediagram = ForceVolMesh.from_vertices_and_cells(vertices, cells)
+
+force_layer = 'force_volmesh'
+forcediagram.layer = force_layer
+forcediagram.attributes['name'] = force_layer
+
+# visualise the force_volmesh
+forcediagramartist = VolMesh3gsArtist(forcediagram, layer=force_layer)
+forcediagramartist.draw()
+
+rs.HideObjects(guids)
 
 
 # ------------------------------------------------------------------------------
@@ -54,7 +78,7 @@ while True:
     # invalid or no modification
     if modify is None or modify == 'exit':
         rs.EnableRedraw(False)
-        forcediagram.draw()
+        forcediagramartist.draw()
         break
 
     # move the selected vertices
@@ -69,11 +93,14 @@ while True:
     # modify fixity of selected vertices
     elif modify == 'fixity':
         rhino_vertex_modify_fixity(forcediagram)
-    
+        
     # visualise the modified force_volmesh
-    forcediagram.draw()
-    draw_vertex_fixities(forcediagram)
+    forcediagramartist.clear()
+    forcediagramartist.draw()
+
+    draw_vertex_fixities(forcediagram, forcediagramartist)
 
     rs.EnableRedraw(True)
 
-forcediagram.draw()
+forcediagramartist.clear()
+forcediagramartist.draw()
