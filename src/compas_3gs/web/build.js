cd8875e829621fbeccb005419df1b70312b6a08f
Li <leochen1990@gmail.com>
2020-11-30 18:02:26 +0100

    dynamically build front-page

--- src/compas_3gs/web/build.js
+++ src/compas_3gs/web/build.js
@@ -7,7 +7,7 @@ let electron_app_path = "electron"
 let temp_path = "temp"
 let app_name = "frontpage"
 let zip_path = 'electron.zip'
-
+let version = process.argv[2]
 
 async function bundleElectronApp(options) {
   const appPaths = await packager(options)
@@ -17,6 +17,14 @@ async function bundleElectronApp(options) {
   fs.renameSync(appPaths[0], electron_app_path)
   fs.rmdirSync(temp_path)
   console.log('moved to '+ electron_app_path)
+
+  if (version){
+    console.log("Set version in index.html with", version)
+    let html_path = electron_app_path+"/resources/app/src/index.html"
+    let html = fs.readFileSync(html_path, "utf-8")
+    html = html.replace("{{dev}}", version)
+    fs.writeFileSync(html_path,html)
+  }
 
   if (process.platform === "darwin") return
   
