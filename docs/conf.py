b8c48b7430dd56ab6e4e292e1d6dc8de6fa66bd0
Li <leochen1990@gmail.com>
2021-05-04 16:14:28 +0200

    Bump version to 0.4.3rc0

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ import sphinx_compas_theme
 project = 'compas_3gs'
 copyright = '2018, Block Research Group - ETH Zurich'
 author = 'Juney Lee'
-release = '0.4.2'
+release = '0.4.3rc0'
 
 version = '.'.join(release.split('.')[0:2])
 
