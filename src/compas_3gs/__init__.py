65e804e0ad53905a2a55f56da247666fb7312fc2
Licini <leochen1990@gmail.com>
2021-09-28 19:34:34 +0200

    Bump version to 0.5.2rc0

--- src/compas_3gs/__init__.py
+++ src/compas_3gs/__init__.py
@@ -27,7 +27,7 @@ __copyright__ = 'Copyright 2018 - Block Research Group, ETH Zurich'
 __license__ = 'MIT License'
 __email__ = 'juney.lee@arch.ethz.ch'
 
-__version__ = '0.5.1'
+__version__ = '0.5.2rc0'
 
 
 HERE = os.path.dirname(__file__)
